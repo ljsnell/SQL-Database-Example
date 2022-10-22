e3d8e50df03cc5ab2ddce2df6e1b9df2ced121dc
ljsnell <ljsnell7@gmail.com>
2022-10-21 23:55:11 -0500

    Updated

--- src/com/example/SelectAllFromCats.java
+++ src/com/example/SelectAllFromCats.java
@@ -6,11 +6,11 @@ public class SelectAllFromCats {
 
   public static void main(String[] args) throws SQLException {
     Connection con = DriverManager.getConnection(
-        "jdbc:sqlserver://localhost:1433;database=cats", "user", "password");
-    PreparedStatement stmt = con.prepareStatement("select * from cat");
+        "jdbc:sqlserver://localhost:1433;database=cats", "CHANGEME", "CHANGEME");
+    PreparedStatement stmt = con.prepareStatement("SELECT * FROM [cats].[dbo].[cat]");
     ResultSet rslt = stmt.executeQuery();
     while (rslt.next()) {
-      System.out.println(rslt.getString("name"));
+      System.out.println(rslt.getString("cat_name"));
     }
   }
 }
